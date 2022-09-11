6036fef7e2e51d6c239894e3586eeffc1c936147
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-11 18:01:47 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'  
+  resources :users, only: :new  
+end
